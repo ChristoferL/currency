496e695651f8922173971703bd5e2c2275403419
Christopher Tamulevich <chris.levich@gmail.com>
2017-07-22 01:47:20 -0400

    Add files via upload

--- crafting.lua
+++ crafting.lua
@@ -1,14 +1,14 @@
 minetest.register_craft({
-	output = 'currency:safe',
+	output = 'uscurrency:safe',
 	recipe = {
 		{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
-		{'default:steel_ingot', 'default:mese_crystal', 'default:steel_ingot'},
+		{'default:steel_ingot', '', 'default:steel_ingot'},
 		{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
 	}
 })
 
 minetest.register_craft({
-	output = 'currency:shop',
+	output = 'uscurrency:shop',
 	recipe = {
 		{'default:sign_wall'},
 		{'default:chest_locked'},
@@ -16,56 +16,263 @@ minetest.register_craft({
 })
 
 minetest.register_craft({
-	output = 'currency:barter',
+	output = 'uscurrency:barter',
 	recipe = {
 		{'default:sign_wall'},
 		{'default:chest'},
 	}
 })
-
+-- Money
+-- Penny
 minetest.register_craft({
 	type = "shapeless",
-	output = "currency:minegeld_5",
-	recipe = {"currency:minegeld", "currency:minegeld", "currency:minegeld", "currency:minegeld", "currency:minegeld"},
+	output = "uscurrency:penny 5",
+	recipe = {"uscurrency:nickel" },
+})
+-- Nickel
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:nickel",
+	recipe = {"uscurrency:penny", "uscurrency:penny", "uscurrency:penny", "uscurrency:penny",  "uscurrency:penny" },
 })
 
 minetest.register_craft({
 	type = "shapeless",
-	output = "currency:minegeld_10",
-	recipe = {"currency:minegeld_5", "currency:minegeld_5"},
+	output = "uscurrency:nickel 2",
+	recipe = {"uscurrency:dime" },
 })
 
 minetest.register_craft({
 	type = "shapeless",
-	output = "currency:minegeld_5 2",
-	recipe = {"currency:minegeld_10"},
+	output = "uscurrency:nickel 5",
+	recipe = {"uscurrency:quarter" },
+})
+
+-- Dime
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:dime",
+	recipe = {"uscurrency:nickel", "uscurrency:nickel" },
 })
 
 minetest.register_craft({
 	type = "shapeless",
-	output = "currency:minegeld 5",
-	recipe = {"currency:minegeld_5"},
+	output = "uscurrency:dime",
+	recipe = {"uscurrency:nickel", "uscurrency:penny", "uscurrency:penny", "uscurrency:penny", "uscurrency:penny", "uscurrency:penny" },
 })
 
 minetest.register_craft({
 	type = "shapeless",
-	output = "currency:minegeld_bundle",
+	output = "uscurrency:dime 7",
+	recipe = {"uscurrency:nickel", "uscurrency:nickel", "uscurrency:nickel", "uscurrency:nickel", "uscurrency:quarter", "uscurrency:quarter" },
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:dime 8",
+	recipe = {"uscurrency:nickel", "uscurrency:nickel", "uscurrency:nickel", "uscurrency:nickel", "uscurrency:nickel", "uscurrency:nickel", "uscurrency:quarter", "uscurrency:quarter" },
+})
+-- Quarter
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:quarter",
+	recipe = {"uscurrency:dime", "uscurrency:penny", "uscurrency:penny", "uscurrency:penny", "uscurrency:penny", "uscurrency:penny", "uscurrency:nickel", "uscurrency:nickel"},
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:quarter",
+	recipe = {"uscurrency:dime", "uscurrency:nickel", "uscurrency:nickel", "uscurrency:nickel"},
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:quarter",
+	recipe = {"uscurrency:nickel", "uscurrency:nickel", "uscurrency:nickel", "uscurrency:nickel", "uscurrency:nickel"},
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:quarter",
+	recipe = {"uscurrency:dime", "uscurrency:dime", "uscurrency:nickel"},
+})
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:quarter",
+	recipe = {"uscurrency:nickel", "uscurrency:nickel", "uscurrency:nickel", "uscurrency:nickel", "uscurrency:penny", "uscurrency:penny", "uscurrency:penny", "uscurrency:penny", "uscurrency:penny"},
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:quarter 2",
+	recipe = {"uscurrency:dime", "uscurrency:dime", "uscurrency:nickel", "uscurrency:dime", "uscurrency:dime", "uscurrency:nickel"},
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:quarter 2",
+	recipe = {"uscurrency:dime", "uscurrency:dime", "uscurrency:dime", "uscurrency:dime", "uscurrency:dime"},
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:quarter 2",
+	recipe = {"uscurrency:dime", "uscurrency:nickel", "uscurrency:nickel", "uscurrency:nickel", "uscurrency:nickel",  "uscurrency:nickel", "uscurrency:nickel", "uscurrency:nickel",  "uscurrency:nickel"},
+})
+
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:dollar_5",
+	recipe = {"uscurrency:dollar", "uscurrency:dollar", "uscurrency:dollar", "uscurrency:dollar", "uscurrency:dollar"},
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:dollar_10",
+	recipe = {"uscurrency:dollar_5", "uscurrency:dollar_5"},
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:dollar_5 2",
+	recipe = {"uscurrency:dollar_10"},
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:dollar 5",
+	recipe = {"uscurrency:dollar_5"},
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:dollar",
+	recipe = {"uscurrency:quarter", "uscurrency:quarter", "uscurrency:quarter", "uscurrency:quarter" },
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:quarter",
+	recipe = {"uscurrency:dime", "uscurrency:dime", "uscurrency:nickel" },
+})
+
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:dime",
+	recipe = {"uscurrency:nickel", "uscurrency:penny", "uscurrency:penny", "uscurrency:penny", "uscurrency:penny", "uscurrency:penny" } 
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:dime",
+	recipe = {"uscurrency:nickel", "uscurrency:nickel"},
+})
+
+
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:dollar_5 2",
+	recipe = {"uscurrency:dollar_10" },
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:dollar_10 2",
+	recipe = {"uscurrency:dollar_20" },
+})
+
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:dollar 5",
+	recipe = {"uscurrency:dollar_5" },
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:dollar 20",
+	recipe = {"uscurrency:dollar_10", "uscurrency:dollar_10" },
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:dollar_10 5",
+	recipe = {"uscurrency:dollar_50" },
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:dollar 50",
+	recipe = {"uscurrency:dollar_10", "uscurrency:dollar_20", "uscurrency:dollar_20" },
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:dollar_100",
+	recipe = {"uscurrency:dollar_50", "uscurrency:dollar_50" },
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:dollar_100",
+	recipe = {"uscurrency:dollar_20", "uscurrency:dollar_20", "uscurrency:dollar_20", "uscurrency:dollar_20", "uscurrency:dollar_20"},
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:dollar_20 5",
+	recipe = {"uscurrency:dollar_100" },
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:dollar_20",
+	recipe = {"uscurrency:dollar_10", "uscurrency:dollar_10" },
+})
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:quarter 4",
+	recipe = {"uscurrency:dollar" },
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:dime 5",
 	recipe = {
-		"group:minegeld",
-		"group:minegeld",
-		"group:minegeld",
-		"group:minegeld",
-		"group:minegeld",
-		"group:minegeld",
-		"group:minegeld",
-		"group:minegeld",
-		"group:minegeld"
+		"uscurrency:quarter", "uscurrency:quarter" 
+	},
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:nickel 5", 
+	recipe = {
+		"uscurrency:quarter" 
 	},
 })
 
 minetest.register_craft({
 	type = "fuel",
-	recipe = "currency:minegeld_bundle",
+	recipe = "uscurrency:dollar_bundle",
 	burntime = 1,
 })
 
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:dollar_500",
+	recipe = {"uscurrency:dollar_100", "uscurrency:dollar_100", "uscurrency:dollar_100", "uscurrency:dollar_100", "uscurrency:dollar_100" },
+})
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:dollar_100 5",
+	recipe = {"uscurrency:dollar_500" },
+})
+minetest.register_craft({
+	type = "shapeless",
+	output = "uscurrency:dollar_1000",
+	recipe = {"uscurrency:dollar_500", "uscurrency:dollar_500" },
+})
--- craftitems.lua
+++ craftitems.lua
@@ -1,30 +1,84 @@
--- internationalization boilerplate
-local MP = minetest.get_modpath(minetest.get_current_modname())
-local S, NS = dofile(MP.."/intllib.lua")
-
-minetest.register_craftitem("currency:minegeld", {
-    description = S("1 MineGeld Note"),
-    inventory_image = "minegeld.png",
-        stack_max = 30000,
-		groups = {minegeld = 1}
+minetest.register_craftitem("uscurrency:penny", {
+    description = "penny",
+    inventory_image = "penny.png",
+        stack_max = 50,
+		groups = {penny = 1}
 })
 
-minetest.register_craftitem("currency:minegeld_5", {
-    description = S("5 MineGeld Note"),
-    inventory_image = "minegeld_5.png",
-        stack_max = 30000,
-		groups = {minegeld = 1}
+minetest.register_craftitem("uscurrency:nickel", {
+    description = "nickel",
+    inventory_image = "nickel.png",
+        stack_max = 40,
+		groups = {penny = 5}
 })
 
-minetest.register_craftitem("currency:minegeld_10", {
-    description = S("10 MineGeld Note"),
-    inventory_image = "minegeld_10.png",
-        stack_max = 30000,
-		groups = {minegeld = 1}
+minetest.register_craftitem("uscurrency:dime", {
+    description = "dime",
+    inventory_image = "dime.png",
+        stack_max = 50,
+		groups = {penny = 10}
 })
 
-minetest.register_craftitem("currency:minegeld_bundle", {
-    description = S("Bundle of random Minegeld notes"),
-    inventory_image = "minegeld_bundle.png",
-        stack_max = 30000,
+minetest.register_craftitem("uscurrency:quarter", {
+    description = "quarter",
+    inventory_image = "quarter.png",
+        stack_max = 40,
+		groups = {penny = 25}
 })
+
+minetest.register_craftitem("uscurrency:dollar", {
+    description = "1 dollar Note",
+    inventory_image = "dollar.png",
+        stack_max = 99,
+		groups = {penny = 100}
+})
+
+minetest.register_craftitem("uscurrency:dollar_5", {
+    description = "5 dollar Note",
+    inventory_image = "dollar_5.png",
+        stack_max = 20,
+		groups = {penny = 500}
+})
+
+minetest.register_craftitem("uscurrency:dollar_10", {
+    description = "10 dollar Note",
+    inventory_image = "dollar_10.png",
+        stack_max = 100,
+		groups = {penny = 1000}
+})
+
+minetest.register_craftitem("uscurrency:dollar_20", {
+    description = "20 dollar Note",
+    inventory_image = "dollar_20.png",
+        stack_max = 40,
+		groups = {penny = 2000}
+})
+
+minetest.register_craftitem("uscurrency:dollar_50", {
+    description = "50 dollar Note",
+    inventory_image = "dollar_50.png",
+        stack_max = 20,
+		groups = {penny = 5000}
+})
+
+minetest.register_craftitem("uscurrency:dollar_100", {
+    description = "100 dollar Note",
+    inventory_image = "dollar_100.png",
+        stack_max = 10,
+		groups = {penny = 10000}
+})
+
+minetest.register_craftitem("uscurrency:dollar_500", {
+    description = "500 dollar Note",
+    inventory_image = "dollar_500.png",
+        stack_max = 10,
+		groups = {penny = 50000}
+})
+
+minetest.register_craftitem("uscurrency:dollar_1000", {
+    description = "1000 dollar Note",
+    inventory_image = "dollar_1000.png",
+        stack_max = 10,
+		groups = {penny = 100000}
+})
+
